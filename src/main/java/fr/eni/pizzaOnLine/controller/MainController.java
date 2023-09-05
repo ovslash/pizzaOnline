65f4d61724a4d06c2d80d95376885cdd5e68e378
Olivier <ovslash@hotmail.fr>
2023-09-05 13:51:18 +0200

    suppression produit ok - pb sur modification

--- src/main/java/fr/eni/pizzaOnLine/controller/MainController.java
+++ src/main/java/fr/eni/pizzaOnLine/controller/MainController.java
@@ -1,6 +1,7 @@
 package fr.eni.pizzaOnLine.controller;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.eni.pizzaOnLine.entity.Produit;
 import jakarta.websocket.server.PathParam;
@@ -18,9 +20,9 @@ public class MainController {
 	private List<Produit> lstproduits = new ArrayList<Produit>();
 
 	public MainController() {
-		Produit produit1 = new Produit(1L, "testNom", "testDesc", 1, "/img/pizza1.jpg");
-		Produit produit2 = new Produit(2L, "testNom2", "testDesc2", 2, "/img/pizza2.jpg");
-		Produit produit3 = new Produit(3L, "testNom3", "testDesc", 3, "/img/pizza3.jpg");
+		Produit produit1 = new Produit(1L, "testNom111", "testDesc111", 1, "/img/pizza1.jpg");
+		Produit produit2 = new Produit(2L, "testNom222", "testDesc222", 2, "/img/pizza2.jpg");
+		Produit produit3 = new Produit(3L, "testNom333", "testDesc333", 3, "/img/pizza3.jpg");
 
 		lstproduits.add(produit1);
 		lstproduits.add(produit2);
@@ -50,13 +52,53 @@ public class MainController {
 	}
 
 	@GetMapping("/carte/supprimer")
-	public String supprimerProduit(@PathParam("id") Long id) {
+	public String supprimerProduit(@RequestParam("id") Long id) {
 
-		System.err.println(id); // OK
-		
-		//lstproduits.remove(-------------);
+//		System.out.println("suppr");
+//		System.err.println(id); // OK
+
+		Produit p = new Produit();
+		p.setId(id);
+		lstproduits.remove(p);
 
 		return "redirect:/carte";
 	}
 
+	@GetMapping("/carte/modifier")
+	public String modifierProduit(@RequestParam("id") Long id, Model model) {
+		
+		Produit p = new Produit();
+
+		for (Produit produit : lstproduits) {
+			if (produit.getId() == id) {
+				p = produit;
+			}
+		}
+
+		model.addAttribute("produit", p);
+		
+		
+
+		return "modifier";
+	}
+	
+	@PostMapping("/carte/modifier")
+	public String modifierProduit( Produit produit) {
+		
+//		System.out.println(produit);
+//		System.out.println("-----------------------------");
+//		System.out.println(lstproduits);
+//		
+//		for (Produit produitOrigine : lstproduits) {
+//			if (produitOrigine.getId() == produit.getId()) {
+//				produitOrigine = produit;
+//			}
+//		}
+		
+		
+		
+		
+		
+		return "redirect:/carte";
+	}
 }
--- src/main/java/fr/eni/pizzaOnLine/entity/Produit.java
+++ src/main/java/fr/eni/pizzaOnLine/entity/Produit.java
@@ -1,5 +1,7 @@
 package fr.eni.pizzaOnLine.entity;
 
+import java.util.Objects;
+
 public class Produit {
 
 	private Long id;
@@ -63,6 +65,23 @@ public class Produit {
 
 	public String getUrlImage() {
 		return urlImage;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produit other = (Produit) obj;
+		return Objects.equals(id, other.id);
 	}
 
 	public void setUrlImage(String urlImage) {
